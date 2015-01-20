abb6fb8aeea6b210dd37c7cb37aa62f8bb5e27fc
ashrithr <ashrith@me.com>
2015-01-20 20:27:18 +0530

    Update nn_backup to implement compression and retention of backup files

--- nn_backup.rb
+++ nn_backup.rb
@@ -8,7 +8,6 @@
 # OPTIMIZE - Make this script auto-detect whether running from namenode
 #            if so, check the edits transactions to copy to instead of
 #            relying on user passing starting and ending transactions
-# OPTIMIZE - Implement compression and retention of backup files
 #
 # Copyright 2013, Cloudwick, Inc.
 #
@@ -30,6 +29,7 @@ require 'timeout'
 require 'fileutils'
 require 'tempfile'
 require 'date'
+require 'zlib'
 
 options = {}
 optparse = OptionParser.new do |opts|
@@ -63,6 +63,11 @@ optparse = OptionParser.new do |opts|
     options[:end_trans_id] = e
   end
 
+  options[:retention_days] = 5
+  opts.on('-r', '--retention-days', Numeric, 'How many days do we have to keep the files, default: 5 days') do |r|
+    options[:retention_days] = r
+  end
+
   opts.on('-h', '--help', 'Display this screen') do
     puts opts
     exit
@@ -85,6 +90,19 @@ rescue OptionParser::InvalidOption, OptionParser::MissingArgument
 end
 
 puts options
+$rwd = Dir.pwd
+puts $rwd
+
+def compress_file (src_file)
+  Zlib::GzipWriter.open("#{File.basename(src_file)}.gz") do |gz|
+   File.open(src_file) do |fp|
+     while chunk = fp.read(16 * 1024) do
+       gz.write chunk
+     end
+   end
+   gz.close
+  end
+end
 
 def check_download_dirs (dirs)
   dirs.each do |dir|
@@ -92,6 +110,21 @@ def check_download_dirs (dirs)
       FileUtils::mkdir_p dir
     end
   end
+end
+
+def file_age (name)
+  (Time.now - File.ctime(name))/(24 * 3600)
+end
+
+def delete_on_retention (dirs, file_pattern, days)
+  # `find -atime +#{days} -name "#{file_regex}" -exec rm {} \\;`
+  dirs.each do |dir|
+    Dir.chdir(dir)
+    Dir.glob(file_pattern).each do |file|
+      File.delete(file) if file_age(file) > days
+    end
+  end
+  Dir.chdir($rwd)
 end
 
 def get_namenode_webpage_contents (namenode_host, namenode_port)
@@ -117,7 +150,7 @@ def get_current_transaction_id (namenode_wp_contents)
   namenode_wp_contents.match(/<b>Current transaction ID:<\/b> (.*)<br\/>/i).captures
 end
 
-def download_fsimage (namenode_host, namenode_port, hadoop_version, download_dirs)
+def download_fsimage (namenode_host, namenode_port, hadoop_version, download_dirs, retention)
   if hadoop_version =~ "1.[0-9].[0-9]"
     puts "hadoop v1"
     uri = "http://#{namenode_host}:#{namenode_port}/getimage?getimage=1"
@@ -129,17 +162,20 @@ def download_fsimage (namenode_host, namenode_port, hadoop_version, download_dir
     uri="http://#{namenode_host}:#{namenode_port}/imagetransfer?getimage=1&txid=latest"
   end
 
-  check_download_dirs (download_dirs)
+  check_download_dirs download_dirs
+  delete_on_retention download_dirs, "fsimage*", retention
 
   time_in_ms = DateTime.now.strftime('%s')
   random_5chars = (0...5).map { ('a'..'z').to_a[rand(26)] }.join
   file_to_write = "fsimage.#{random_5chars}.#{time_in_ms}"
+  file_to_write_gz = "fsimage.#{random_5chars}.#{time_in_ms}.gz"
   begin
     open(file_to_write, 'wb') do |file|
       file << open(uri, 'rb').read
     end
+    compress_file file_to_write
     download_dirs.each do |dir|
-      FileUtils.cp(file_to_write, dir)
+      FileUtils.cp(file_to_write_gz, dir)
     end
   rescue Timeout::Error
     puts "The request for a page at #{uri} timed out...exiting."
@@ -150,10 +186,11 @@ def download_fsimage (namenode_host, namenode_port, hadoop_version, download_dir
     exit
   ensure
     FileUtils.rm(file_to_write)
+    FileUtils.rm(file_to_write_gz)
   end
 end
 
-def download_edits (namenode_host, namenode_port, hadoop_version, start_trans_id, end_trans_id, download_dirs)
+def download_edits (namenode_host, namenode_port, hadoop_version, start_trans_id, end_trans_id, download_dirs, retention)
   if hadoop_version =~ "1.[0-9].[0-9]"
     uri = "http://#{namenode_host}:#{namenode_port}/getimage?getedit=1"
   elsif hadoop_version =~ "2.[0-3].[0-9]"
@@ -162,16 +199,19 @@ def download_edits (namenode_host, namenode_port, hadoop_version, start_trans_id
     uri="http://#{namenode_host}:#{namenode_port}/imagetransfer?getedit=1&startTxId=#{start_trans_id}&endTxId=#{end_trans_id}"
   end
 
-  check_download_dirs (download_dirs)
+  check_download_dirs download_dirs
+  delete_on_retention download_dirs, "edits*", retention
 
   time_in_ms = DateTime.now.strftime('%s')
   file_to_write = "edits.#{start_trans_id}.#{end_trans_id}.#{time_in_ms}"
+  file_to_write_gz = "edits.#{start_trans_id}.#{end_trans_id}.#{time_in_ms}.gz"
   begin
     open(file_to_write, 'wb') do |file|
       file << open(uri, 'rb').read
     end
+    compress_file file_to_write
     download_dirs.each do |dir|
-      FileUtils.cp(file_to_write, dir)
+      FileUtils.cp(file_to_write_gz, dir)
     end
   rescue Timeout::Error
     puts "The request for a page at #{uri} timed out...exiting."
@@ -182,6 +222,7 @@ def download_edits (namenode_host, namenode_port, hadoop_version, start_trans_id
     exit
   ensure
     FileUtils.rm(file_to_write)
+    FileUtils.rm(file_to_write_gz)
   end
 end
 
@@ -190,5 +231,5 @@ nn_wb_cnts = get_namenode_webpage_contents(options[:namenode], options[:port])
 hadoop_version = get_hadoop_version(nn_wb_cnts)
 current_trans_id = get_current_transaction_id(nn_wb_cnts).first.to_i
 
-download_fsimage(options[:namenode], options[:port], hadoop_version, options[:dirs])
-download_edits(options[:namenode], options[:port], hadoop_version, options[:start_trans_id], options[:end_trans_id], options[:dirs])
+download_fsimage(options[:namenode], options[:port], hadoop_version, options[:dirs], options[:retention_days])
+download_edits(options[:namenode], options[:port], hadoop_version, options[:start_trans_id], options[:end_trans_id], options[:dirs], options[:retention_days])
